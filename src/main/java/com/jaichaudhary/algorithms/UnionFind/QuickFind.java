99e92f290acbdd9f650994896aebcef9188e328b
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-07-07 20:05:42 -0400

    Added union find interface and a quick find implementation

---
+++ src/main/java/com/jaichaudhary/algorithms/UnionFind/QuickFind.java
@@ -0,0 +1,50 @@
+public class QuickFind 
+	implements UnionFind {
+	private int[] id;
+	private int count;
+
+	public QuickFind(int N){
+		count = N;
+		id = new int[N];
+		for(int i = 0; i < N; i++) id[i] = i;
+	}
+
+	public int count(){
+		return count;
+	}
+
+	public boolean connected(int p, int q) {
+		return id[p] == id[q];
+	}
+
+	public int find(int p){
+		return id[p];
+	}
+
+	public void union(int p,int q) {
+		int pid = id[p];
+		int qid = id[q];
+		if(pid != qid) {
+			for(int i = 0; i < id.length; i++) {
+				if (id[i] == pid) {
+					id[i] = qid;
+				}
+			}
+			count--;			
+		}
+
+	}
+
+	public static void main(String[] args) {
+		int N = StdIn.readInt();
+		UF uf = new UF(N);
+		while (!StdIn.isEmpty()) {
+			int p = StdIn.readInt();
+			int q = StdIn.readInt();
+			if (!uf.connected(p, q)) {
+				 uf.union(p, q);
+				 StdOut.println(p + " " + q);
+			}
+		}
+	}
+}
---
+++ src/main/java/com/jaichaudhary/algorithms/UnionFind/UnionFind.java
@@ -0,0 +1,6 @@
+public interface UnionFind {
+	public void union(int p, int q);
+	public boolean connected(int p, int q);
+	public int find(int p);
+	public int count();
+}
