93dbec34e305a16d2d44d1891cd09345ce187139
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-07-07 21:55:58 -0400

    Fixed Client function in QuickFind to call QuickFind instead of UF

--- src/main/java/com/jaichaudhary/algorithms/UnionFind/QuickFind.java
+++ src/main/java/com/jaichaudhary/algorithms/UnionFind/QuickFind.java
@@ -37,7 +37,7 @@ public class QuickFind
 
 	public static void main(String[] args) {
 		int N = StdIn.readInt();
-		UF uf = new UF(N);
+		UnionFind uf = new QuickFind(N);
 		while (!StdIn.isEmpty()) {
 			int p = StdIn.readInt();
 			int q = StdIn.readInt();
