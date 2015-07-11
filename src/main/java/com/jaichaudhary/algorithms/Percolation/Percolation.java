3610d68374b5e812d530aecf9c13e37d63c35e83
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-07-10 23:48:53 -0400

    Added Exceptions if any argument to open(), isOpen(), or isFull() is outside its prescribed range

--- src/main/java/com/jaichaudhary/algorithms/Percolation/Percolation.java
+++ src/main/java/com/jaichaudhary/algorithms/Percolation/Percolation.java
@@ -1,9 +1,38 @@
 public class Percolation {
-   public Percolation(int N)               // create N-by-N grid, with all sites blocked
-   public void open(int i, int j)          // open site (row i, column j) if it is not open already
-   public boolean isOpen(int i, int j)     // is site (row i, column j) open?
-   public boolean isFull(int i, int j)     // is site (row i, column j) full?
-   public boolean percolates()             // does the system percolate?
+    private int[][] grid;
 
-   public static void main(String[] args   // test client (optional)
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; i < N; i++) {
+                grid[i][j] = 1;
+            }   
+        }
+    } 
+
+
+    // open site (row i, column j) if it is not open already
+    public void open(int i, int j) throws IndexOutOfBoundsException {
+
+    }
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) throws IndexOutOfBoundsException {
+        return false;
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) throws IndexOutOfBoundsException {
+        return false;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return false;
+    }
+    
+    // test client (optional)
+    public static void main(String[] args) {
+
+    }
 }
