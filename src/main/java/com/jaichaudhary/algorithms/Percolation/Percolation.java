6be09435c26b44b080d062db77c2be6f5993a1fb
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-08-28 14:30:49 -0400

    Implemented Percolation simulation with Union Find

--- src/main/java/com/jaichaudhary/algorithms/Percolation/Percolation.java
+++ src/main/java/com/jaichaudhary/algorithms/Percolation/Percolation.java
@@ -2,6 +2,7 @@ package com.jaichaudhary.algorithms.Percolation;
 
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.UF;
 
@@ -9,25 +10,47 @@ import com.jaichaudhary.algorithms.UnionFind.*;
 
 public class Percolation {
     private int[][] grid;
+    private WeightedQuickUnionUF uf;
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
+        grid = new int[N][N];
         for(int i = 0; i < N; i++) {
-            for(int j = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
                 grid[i][j] = 1;
-            }   
+            }
         }
-    } 
+
+        // generate union find representation from grid
+        uf = new WeightedQuickUnionUF(N * N);
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(isOpen(i, j)) {
+                    if(i < (N - 1) && isOpen(i + 1, j))    uf.union(gridToArray(i, j), gridToArray(i + 1, j));
+                    if (j < (N - 1) && isOpen(i, j + 1))   uf.union(gridToArray(i, j), gridToArray(i, j + 1));
+                }
+            }
+        }
+    }
 
     private boolean indicesInBound(int i, int j) {
         return ( i < grid.length && i >= 0) && ( j < grid[0].length && j >= 0);
     }
 
+    private int gridToArray(int i, int j) {
+        int columns = grid[0].length;
+        return columns * i + j;
+    }
+
     // open site (row i, column j) if it is not open already
     public void open(int i, int j) throws IndexOutOfBoundsException {
         if (indicesInBound(i, j)) {
-            if (isFull(i, j)) {
-                grid[i][j] = 1;
+            if (!isOpen(i, j)) {
+                grid[i][j] = 0;
+                if(i > 0 && isOpen(i - 1, j))   uf.union(gridToArray(i, j), gridToArray(i - 1, j));
+                if(j > 0 && isOpen(i, j - 1))   uf.union(gridToArray(i, j), gridToArray(i, j - 1));
+                if(i < grid.length - 1 && isOpen(i + 1, j))   uf.union(gridToArray(i, j), gridToArray(i + 1, j));
+                if(j < grid[0].length - 1 && isOpen(i, j + 1))   uf.union(gridToArray(i, j), gridToArray(i, j + 1));
             }
         } else {
             throw new IndexOutOfBoundsException();
@@ -38,7 +61,7 @@ public class Percolation {
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) throws IndexOutOfBoundsException {
         if (indicesInBound(i, j)) {
-            return grid[i][j] == 1;
+            return grid[i][j] == 0;
         } else {
             throw new IndexOutOfBoundsException();
         }
@@ -46,9 +69,15 @@ public class Percolation {
     }
 
     // is site (row i, column j) full?
+    // Full site is an open site that can be connected to an open site in the top row via a chain of neighboring (left, right, up, down) open sites.
     public boolean isFull(int i, int j) throws IndexOutOfBoundsException {
         if (indicesInBound(i, j)) {
-            return grid[i][j] == 0;
+            for(int k = 0; k < grid[0].length; k++) {
+                if(uf.connected(gridToArray(i,j), gridToArray(0, k))) {
+                    return true;
+                }
+            }
+            return false;
         } else {
             throw new IndexOutOfBoundsException();
         }
@@ -56,12 +85,26 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(1);
+        // check if the system percolates
+        int bottomRow = grid[0].length - 1;
+        for(int j = 0; j < grid.length; j++) {
+            if(isFull(bottomRow, j)) {
+                return true;
+            }
+        }
+
         return false;
     }
     
     // test client (optional)
     public static void main(String[] args) {
-
+        Percolation simulation = new Percolation(2);
+        StdOut.println(simulation.percolates());
+        simulation.open(0,0);
+        StdOut.println(simulation.percolates());
+        simulation.open(0,1);
+        StdOut.println(simulation.percolates());
+        simulation.open(1,1);
+        StdOut.println(simulation.percolates());
     }
 }
