a3c41b65e837b31d6269b357dd8fe4faad817c70
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-08-30 22:40:11 -0400

    Added Stack of Strings Linke List API

--- src/main/java/com/jaichaudhary/algorithms/collections/StackOfStringsLinkedList.java
+++ src/main/java/com/jaichaudhary/algorithms/collections/StackOfStringsLinkedList.java
@@ -1,1 +1,31 @@
 package com.jaichaudhary.algorithms.collections;
+
+
+public class StackOfStringsLinkedList {
+
+	private class Node {
+		String item;
+		Node next;
+	}
+
+	public StackOfStringsLinkedList() {
+
+	}
+
+	public void push(String input) {
+
+	}
+
+	public String pop() {
+
+	}
+
+	public boolean isEmpty() {
+
+	}
+
+	public int size() {
+		
+	}
+
+}
