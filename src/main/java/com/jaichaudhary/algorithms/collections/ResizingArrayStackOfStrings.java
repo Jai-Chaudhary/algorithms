4329de3e668129479aa3823c44ad351a16d0895e
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-08-31 00:37:09 -0400

    Implemented Resizing Array Stack Of Strings APi

--- src/main/java/com/jaichaudhary/algorithms/collections/ResizingArrayStackOfStrings.java
+++ src/main/java/com/jaichaudhary/algorithms/collections/ResizingArrayStackOfStrings.java
@@ -1,17 +1,30 @@
 package com.jaichaudhary.algorithms.collections;
 
-public class FixedCapacityStackOfStrings {
+public class ResizingArrayStackOfStrings {
 
 	private String[] arr = new String[1];
 	private int size = 0;
 
 	public void push(String item) {
-		arr[size++] = item;
+		if( size == arr.length) {
+			String[] newArr = new String[arr.length * 2];
+			for (int i = 0; i < arr.length ; i++) {
+				newArr[i] = arr[i];
+			}
+			arr = newArr;
+		}
+			arr[size++] = item;
 	}
 
 	public String pop() throws IndexOutOfBoundsException{
 		if (isEmpty()) {
 			throw new IndexOutOfBoundsException();
+		} else if (size <= 1 / 4 * arr.length) {
+			String[] newArr = new String[arr.length / 4];
+			for (int i = 0; i < arr.length ; i++) {
+				newArr[i] = arr[i];
+			}
+			arr = newArr;
 		}
 		String item = arr[--size];
 		arr[size] = null;
