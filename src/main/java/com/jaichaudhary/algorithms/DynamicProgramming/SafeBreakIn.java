ea68747fba33077416804ed7acb245b6b24e2810
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-09-02 23:39:09 -0400

    Added ArrayList DS

--- src/main/java/com/jaichaudhary/algorithms/DynamicProgramming/SafeBreakIn.java
+++ src/main/java/com/jaichaudhary/algorithms/DynamicProgramming/SafeBreakIn.java
@@ -1,5 +1,7 @@
 package com.jaichaudhary.algorithms.DynamicProgramming;
 
+import java.util.*;
+
 public class SafeBreakIn {
 	public static void main(String[] args) {
 		int[] digits = {0 , 1};
@@ -7,6 +9,14 @@ public class SafeBreakIn {
 
 		//calculating sequences with most overlapping numbers with n digits
 		// E.g. 0010 4 digits 3 overlapping UNIQUE numbers of 2 digits - 00 01 10
+
+		String[] numbers = {"00", "01", "10", "11"};
+		int maxCountOfOverlappingNums = 1;
+		ArrayList<ArrayList<String>> sequencesWithMostNums = new ArrayList<ArrayList<String>>();
+
+		while(maxCountOfOverlappingNums < 4) {
+			
+		}
 
 	}
 } 
