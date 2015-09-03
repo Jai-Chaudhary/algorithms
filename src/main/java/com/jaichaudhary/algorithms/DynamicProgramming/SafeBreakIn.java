6dcc2fc8378a72ffd71c404afffa64bb651d51c4
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-09-02 23:23:16 -0400

    Simplified problem to binary digits and 2 bits

--- src/main/java/com/jaichaudhary/algorithms/DynamicProgramming/SafeBreakIn.java
+++ src/main/java/com/jaichaudhary/algorithms/DynamicProgramming/SafeBreakIn.java
@@ -2,6 +2,7 @@ package com.jaichaudhary.algorithms.DynamicProgramming;
 
 public class SafeBreakIn {
 	public static void main(String[] args) {
-		
+		int[] digits = {0 , 1};
+		int mostSignificantBit = 2;
 	}
 } 
