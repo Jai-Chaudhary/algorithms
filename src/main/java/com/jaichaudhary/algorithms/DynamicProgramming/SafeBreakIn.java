f6651c26ca6a7a1760da88a51a0c5e11bd453ba5
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-09-02 23:25:46 -0400

    Added comment for approach to DP problem

--- src/main/java/com/jaichaudhary/algorithms/DynamicProgramming/SafeBreakIn.java
+++ src/main/java/com/jaichaudhary/algorithms/DynamicProgramming/SafeBreakIn.java
@@ -4,5 +4,9 @@ public class SafeBreakIn {
 	public static void main(String[] args) {
 		int[] digits = {0 , 1};
 		int mostSignificantBit = 2;
+
+		//calculating sequences with most overlapping numbers with n digits
+		// E.g. 0010 4 digits 3 overlapping UNIQUE numbers of 2 digits - 00 01 10
+
 	}
 } 
