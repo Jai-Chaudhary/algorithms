5131fd1c9bb74adea6a3040285e12c8a59950f6d
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-09-08 23:41:37 -0400

    Implemented Insertion Sort API

--- src/main/java/com/jaichaudhary/algorithms/Sorts/InsertionSort.java
+++ src/main/java/com/jaichaudhary/algorithms/Sorts/InsertionSort.java
@@ -4,11 +4,24 @@ package com.jaichaudhary.algorithms.Sorts;
 public class InsertionSort {
 
 	private InsertionSort() {
-
 	}
-	
-	public static void sort() {
 
-	} 
+	public static void sort(Comparable[] a) {
+		for(int i = 0; i < a.length; i++) {
+			for(int j = i ; j >= 0 && less(a[i], a[j]) ; j--) {
+				exch(a, j, i);
+			}
+		}
+	}
+
+	private static boolean less(Comparable u, Comparable v) {
+		return u.compareTo(v) < 0;
+	}
+
+	private static void exch(Object[] a, int i, int j) {
+		Object temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
 
 }
