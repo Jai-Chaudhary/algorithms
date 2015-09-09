af5907da752a564759d9249ce9e0b92475584e89
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-09-08 23:50:56 -0400

    Added private helper methods in Merge Sort

--- src/main/java/com/jaichaudhary/algorithms/Sorts/MergeSort.java
+++ src/main/java/com/jaichaudhary/algorithms/Sorts/MergeSort.java
@@ -9,4 +9,14 @@ class MergeSort() {
 	public static void sort(Comparable[] a, Comparable[] aux, int lo, int hi){
 
 	}
+
+	private static boolean less(Comparable u, Comparable v) {
+        return u.compareTo(v) > 0;
+    }
+
+    private static void exch(Object[] a, int u, int v) {
+        Object temp = a[u];
+        a[u] = a[v];
+        a[v] = temp;
+    } 
 }
