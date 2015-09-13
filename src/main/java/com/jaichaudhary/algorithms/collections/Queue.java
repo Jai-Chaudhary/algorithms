be4b038c59aad3b6ec69808618b1afe01f97fd3d
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-09-13 00:13:32 -0400

    Implemented Iteratable in Generic Queue

--- src/main/java/com/jaichaudhary/algorithms/collections/Queue.java
+++ src/main/java/com/jaichaudhary/algorithms/collections/Queue.java
@@ -3,7 +3,10 @@ package com.jaichaudhary.algorithms.collections;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-public class Queue<Item> {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Queue<Item> implements Iterable<Item> {
 
 	private Item[] arr = (Item[]) new Object[2];
 	private int size = 0;
@@ -19,14 +22,14 @@ public class Queue<Item> {
 		if( last == arr.length) last = 0;
 	}
 
-	public Item dequeue() throws IndexOutOfBoundsException{
-		if(isEmpty())	throw new IndexOutOfBoundsException("Queue underflow");
+	public Item dequeue() throws NoSuchElementException{
+		if(isEmpty())	throw new NoSuchElementException("Queue underflow");
 		Item item = arr[first];
 		arr[first] = null;
 		size--;
 		first++;
 		if( first == arr.length)	first = 0;
-		if(arr.length <=  size / 4) {
+		if(size > 0 && size == arr.length / 4) {
 			resize(arr.length / 2);
 		}
 		return item;
@@ -50,14 +53,39 @@ public class Queue<Item> {
 		return size == 0;
 	}
 
+	public Iterator<Item> iterator() {
+		return new ArrayIterator();
+	}
+
+	private class ArrayIterator implements Iterator<Item> {
+		private int i = 0;
+
+		public boolean hasNext() {
+			return  i < size  ;
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+
+		public Item next() {
+			if (!hasNext()) throw new NoSuchElementException();
+			Item item = arr[(i + first) % arr.length];
+			i++;
+			return item;
+		}
+
+	}
+
+
 	public static void main(String[] args) {
-		Queue qos = new Queue<String>();
+		Queue<String> qos = new Queue<String>();
 		while (!StdIn.isEmpty()) {
 			String item = StdIn.readString();
 			if (item.equals("-")) {
 				StdOut.println(qos.dequeue());
 			} else if (item.equals("--")) {
-				while(!qos.isEmpty()){
+				for(String s: qos) {
 					StdOut.println(qos.dequeue());
 				}
 			} else {
