8cbe20940f33dc67b2bc6a391a2d6d8830499fdd
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-09-13 12:54:42 -0400

    Implemented knuth shuffle

--- src/main/java/com/jaichaudhary/algorithms/sort/ShuffleSort.java
+++ src/main/java/com/jaichaudhary/algorithms/sort/ShuffleSort.java
@@ -1,5 +1,37 @@
 package com.jaichaudhary.algorithms.sort;
 
-public class ShuffleSort{
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
+public class ShuffleSort{
+	private ShuffleSort() {};
+
+	public static void shuffle(Comparable[] a) {
+		for(int i = 1; i < a.length; i++) {
+			int randomIndex = StdRandom.uniform(i);
+			exch(a, i, randomIndex);
+		}
+	}
+
+	private static void exch(Comparable[] a, int i, int j) {
+		Comparable swap = a[i];
+		a[i] = a[j];
+		a[j] = swap;
+	}
+
+    private static void display(Comparable[] a){
+        StdOut.println("Shuffled: ");
+        for(int  i = 0; i < a.length; i++) {
+            StdOut.println(a[i]);
+        }
+    }
+
+	public static void main(String[] args) {
+		int N = StdIn.readInt();
+		String[] input = new String[N];
+		for(int i = 0; i < N; i++)	input[i] =  StdIn.readString();
+		ShuffleSort.shuffle(input);
+		display(input);
+	}
 }
