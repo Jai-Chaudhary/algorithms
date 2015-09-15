415c46284d73b654d705ee8cf4a99603e59bc6c8
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-09-14 22:45:31 -0400

    Implementing overloaded sort method for simpler API

--- src/main/java/com/jaichaudhary/algorithms/sort/MergeSort.java
+++ src/main/java/com/jaichaudhary/algorithms/sort/MergeSort.java
@@ -6,20 +6,31 @@ class MergeSort() {
 
 	}
 
-	public static void sort(Comparable[] a, int lo, int hi){
+	public static void sort(Comparable[] a){
+		Comparable[] aux = new Comparable[a.length];
+		sort(a, aux, 0, a.length - 1);
+	}
+
+	private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi){
 		for(int i = lo; i < hi; i++){
 			aux[i] = a[i];
 		}
 
 		int mid = (lo + hi) / 2;
+
 		sort(a, aux, mid, hi);
-		sort(a, aux, lo, mid);
+		sort(a, aux, lo, mid)
 		return merge(a, aux, lo, hi);
 
 	}
 
 	private static void merge(Comparable[] a, Comparable[] aux, int lo, int hi) {
-		
+		int i = 0;
+		int j = 0;
+		int mid = (lo + hi) / 2;
+		while() {
+			if(i >= mid) a[lo]
+		}
 	}
 
 	private static boolean less(Comparable u, Comparable v) {
