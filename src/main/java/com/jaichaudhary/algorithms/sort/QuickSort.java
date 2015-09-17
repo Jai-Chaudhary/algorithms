97bd292dfe6eb4f41752124dc74ce026e1f77cc0
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2015-09-16 23:42:43 -0400

    Quicksort test client without API

--- src/main/java/com/jaichaudhary/algorithms/sort/QuickSort.java
+++ src/main/java/com/jaichaudhary/algorithms/sort/QuickSort.java
@@ -1,7 +1,26 @@
 package com.jaichaudhary.algorithms.sort;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
 class QuickSort {
 	private QuickSort(){
 		
 	}
+
+	public static void sort(Comparable[] a) {
+		
+	}
+
+	   // Test Client
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        Double[] input = new Double[N];
+        for(int i = 0; i < N; i++) {
+            input[i] = StdRandom.uniform();
+        }
+        QuickSort.sort(input);
+        display(input);
+    }
 }
